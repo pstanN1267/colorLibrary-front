234f1444fa779b160a76e0084ab10a0c5eca2d07
pstanN1267 <piotr.stnk@gmail.com>
2024-09-09 22:05:56 +0200

    konfiguracja projektu

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,35 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideIcons, provideNgIconsConfig } from '@ng-icons/core';
+import {
+  remixPaintBrushFill,
+  remixPaintBrushLine,
+  remixPaintFill,
+  remixPaintLine,
+  remixPantoneFill,
+  remixPantoneLine,
+} from '@ng-icons/remixicon';
+import {
+  hugePaintBoard,
+  hugePaintBrush01,
+  hugePaintBrush04,
+} from '@ng-icons/huge-icons';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideNgIconsConfig({ size: '1.5em' }),
+    provideIcons({
+      remixPaintBrushFill,
+      remixPaintBrushLine,
+      remixPaintFill,
+      remixPaintLine,
+      remixPantoneFill,
+      remixPantoneLine,
+      hugePaintBoard,
+      hugePaintBrush01,
+      hugePaintBrush04,
+    }),
+  ],
 };
