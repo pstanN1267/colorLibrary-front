c6758a25567c1900e4771d50dcf5447552c0d658
pstanN1267 <piotr.stnk@gmail.com>
2024-09-11 00:05:34 +0200

    responsywnosc home, działanie linku do zakładki

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { AddpaintComponent } from './addpaint/addpaint.component';
 export const routes: Routes = [
   {path:'', component: HomeComponent, pathMatch:'full'},
   {path:'addpaint',component: AddpaintComponent},
-  // {path:'/delete/{id}',component: Deletepaint}, chyba dobrze :D
+  // {path:'delete/{id}',component: Deletepaint}, chyba dobrze :D
   // {path:'userpaints',component: YourLibrary},
   // {path:'stockpaints',component: Library},
 
