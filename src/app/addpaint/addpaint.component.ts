4e48564deaf78a0b030c4d97a68f3cbca7273423
pstanN1267 <piotr.stnk@gmail.com>
2024-09-10 23:17:02 +0200

    responsywnosc home, działanie linku do zakładki

---
+++ src/app/addpaint/addpaint.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addpaint',
+  standalone: true,
+  imports: [],
+  templateUrl: './addpaint.component.html',
+  styleUrl: './addpaint.component.css'
+})
+export class AddpaintComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AddpaintComponent } from './addpaint/addpaint.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component: HomeComponent, pathMatch:'full'},
+  {path:'addpaint',component: AddpaintComponent},
+  // {path:'/delete/{id}',component: Deletepaint}, chyba dobrze :D
+  // {path:'userpaints',component: YourLibrary},
+  // {path:'stockpaints',component: Library},
+
+];
